058374c046ba985582673a4238c9d14728fb4a95
Damien <damien@dam.io>
2016-09-04 13:45:44 +0200

    Writing your first Django app, part 7

--- polls/models.py
+++ polls/models.py
@@ -11,6 +11,9 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
     def __str__(self):
         return self.question_text
